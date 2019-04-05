8a75b4c973f6d702d043ca8f43d7ad6471facbe2
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-05 13:09:17 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,5 +4,6 @@ def alphabetize(arr)
   esp_alp = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esp_map = esp_alp.each_char.with_index.to_a.to_h
 
-  arr.sort_by {|word| esp_map[word]}
+  arr.sort_by {|word| esp_map[word[0]]}
+  arr
 end
