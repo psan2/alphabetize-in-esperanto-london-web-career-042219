e43cbe021be334f9bedebff34011aa3ea2cb4242
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-05 13:23:45 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esp_alp = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esp_map = esp_alp.each_char.to_a
+
+  arr.sort_by do |word|
+    word.split("").map do |letter|
+      esp_map.index(letter)
+    end
+  end
 end
