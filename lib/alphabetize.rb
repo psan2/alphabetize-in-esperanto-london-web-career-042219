bae86e8829532aee2363821f58e416a988cb2166
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-05 13:06:08 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def alphabetize(arr)
   esp_alp = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  esp_map = esp_alp.each_char.with_index.to_a
+  esp_map = esp_alp.each_char.with_index.to_a.to_h
   binding.pry
 end
