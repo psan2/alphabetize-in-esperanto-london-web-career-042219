d3d42b895ce8709b9eec112b0cf1eef9979445ed
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-05 13:14:13 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 def alphabetize(arr)
   esp_alp = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  esp_map = esp_alp.each_char.with_index.to_a.to_h
+  # esp_map = esp_alp.each_char.with_index.to_a.to_h
+  # arr.sort_by {|word| esp_map[word[0]]}
 
-  arr.sort_by {|word| esp_map[word[0]]}
+  ascii = "@-\\"
+  arr.sort_by{|string| string.tr(esp_alp,ascii)}
   arr
 end
